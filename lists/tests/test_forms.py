f74cc0ca9235f4eacafe2df552f15be4fc6259b0
Bob <robertjosephmorris@gmail.com>
2021-03-23 16:13:09 -0400

    use form in all views, back to working state

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,12 +1,9 @@
 from django.test import TestCase
+from lists.models import Item, List
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 
 
 class ItemFormTest(TestCase):
-
-	def test_form_renders_item_text_input(self):
-		form = ItemForm()
-		self.fail(form.as_p())
 
 	def test_form_item_input_has_placeholder_and_css_classes(self):
 		form = ItemForm()
@@ -17,3 +14,11 @@ class ItemFormTest(TestCase):
 		form = ItemForm(data={'text': ''})
 		self.assertFalse(form.is_valid())
 		self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+	
+	def test_form_save_handles_saving_to_a_list(self):
+		list_ = List.objects.create()
+		form = ItemForm(data={'text': 'do me'})
+		new_item = form.save(for_list=list_)
+		self.assertEqual(new_item, Item.objects.first())
+		self.assertEqual(new_item.text, 'do me')
+		self.assertEqual(new_item.list, list_)
