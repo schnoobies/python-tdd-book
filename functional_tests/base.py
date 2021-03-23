201f2c185c04fe0ddbd02cc69b227480f28095eb
Bob <robertjosephmorris@gmail.com>
2021-03-22 21:03:22 -0400

    first-cut FT to prevent blank entries in the list

---
+++ functional_tests/base.py
@@ -0,0 +1,42 @@
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+import time
+
+
+MAX_WAIT = 10
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		staging_server = os.environ.get('STAGING_SERVER')
+		
+		if staging_server:
+			self.live_server_url = 'http://' + staging_server
+		
+	def tearDown(self):
+		self.browser.quit()
+		
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				return fn()
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise effect
+				time.sleep(0.5)
+	
+	def wait_for(self, fn):
+		start_time = time.time()
+		while True:
+			try:
+				return fn()
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
